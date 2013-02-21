b30f430f11ee501f4219b02a2449757f0ecbc6b0
Marcel Hollerbach <marcel.hollerbach@stzedn.de>
2013-02-21 20:44:44 +0100

    Added a Description and a constant to define where your template.xml is located. Also added the function that the flag boolean fields are not printed in the xml code.

--- PhaenovumUserEditor/data/Template.php
+++ PhaenovumUserEditor/data/Template.php
@@ -1,7 +1,18 @@
 <?php
+define("template_file","../config/settings.ini");
+/**
+ * A Class to handle the Template File
+ * @author Marcel Hollerbach
+ *
+*/
 class Template {
+	//array for the fields in the xml file
 	private $attributes;
+	//the raw text from the xmlfile
 	private $text;
+	/**
+	 * Will read in the xml template file.
+	 */
 	function __construct(){
 		//read in the template.xml file
 		$this ->text = file_get_contents('../config/template.xml');
@@ -12,18 +23,41 @@ class Template {
 					$attribute['FLAGS'], $attribute['DESCRIPTION']);
 		}
 	}
+	/**
+	 * Will return the array with the parsed array
+	 * @return Type:Array Will return an Array with objects of the type Attribute
+	 */
 	function getAttributes(){
 		return $this -> attributes;
 	}
+	/**
+	 * Returns the raw data of the template.xml
+	 * @return string
+	 */
 	function getXMLRawData(){
 		return $this->text;
 	}
+	/**
+	 * Will return true if everything is okay with the file otherwise not
+	 */
+	static function testTemplate(){
+		if(file_exists(template_file)&&is_readable(template_file))
+			return true;
+		else{
+			echo "Exists : ".file_exists(template_file)."\n";
+			echo "Is Readable : ".is_readable(template_file)."\n";
+		}
+	}
+	/**
+	 * Parses the given string for String
+	 * @param string $XML
+	 * @return array
+	 */
 	static function XMLtoArray($XML)
 	{
 		$xml_parser = xml_parser_create();
 		xml_parse_into_struct($xml_parser, $XML, $vals);
 		xml_parser_free($xml_parser);
-		// wyznaczamy tablice z powtarzajacymi sie tagami na tym samym poziomie
 		$_tmp='';
 		foreach ($vals as $xml_elem) {
 			$x_tag=$xml_elem['tag'];
@@ -41,7 +75,6 @@ class Template {
 				$_tmp=$x_tag;
 			}
 		}
-		// jedziemy po tablicy
 		foreach ($vals as $xml_elem) {
 			$x_tag=$xml_elem['tag'];
 			$x_level=$xml_elem['level'];
@@ -87,14 +120,27 @@ class Template {
 		return $xml_array;
 	}
 }
+/**
+ * A Class to represent the Attributes which should be shown.
+ * @author Marcel Hollerbach
+ *
+ */
 class Attribute {
 	private $data;
+	private $flagsarray = array("pw","ro","sh");
 	function __construct($ldapname,$htmlalias,$displayname,$flags,$description){
 		$this -> data = array();
 		$this ->data['ldapname'] = $ldapname;
 		$this ->data['displayname'] = $displayname;
 		$this ->data['flags'] = $flags;
-		$this ->data['description'] = $description;
+		foreach ($this->flagsarray as $flagarray){
+			if(strpos($flags,$flagarray) === false){
+				$this ->data[$flagarray] = FALSE;
+			}else{
+				$this ->data[$flagarray] = TRUE;
+			}
+		}
+		$this ->data['description'] = $descripwtion;
 		$this ->data['aliashtmlname'] = $htmlalias;
 	}
 	function getLDAPName(){
@@ -106,13 +152,32 @@ class Attribute {
 	function getFlags(){
 		return $this ->data['flags'];
 	}
+	function isPw(){
+		return $this ->data['pw'];
+	}
+	function isRo(){
+		return $this ->data['ro'];
+	}
+	function isSh(){
+		return $this ->data['sh'];
+	}
 	function getDescription(){
 		return $this ->data['description'];
 	}
+	/**
+	 * Will return the content of this object in xml style
+	 * @return string
+	 */
 	function getXMLContent(){
 		$result = "";
 		foreach ($this ->data as $key => $value){
-			$result .= "<$key>$value</$key>\n";
+			$use = true;
+			foreach ($this->flagsarray as $possibleflag){
+				if($key == $possibleflag)
+					$use = false;
+			}
+			if($use)
+				$result .= "<$key>$value</$key>\n";
 		}
 		return $result;
 	}
